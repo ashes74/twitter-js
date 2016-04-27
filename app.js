63e593d9e4d670d34639122e51f7c8a78535c194
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-04-27 17:22:01 -0400

    And so it begins...

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+
+app.use(morgan(':method :url :status'));
+
+app.use(function (req, res, next) {
+    // do your logging here
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+    console.log(req.method, req.url)
+    next();
+})
+app.use("/special/", function (req, res, next){
+    console.log("You reached the special area");
+    next();
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World!!!!!!!');
+});
+
+app.get('/news', function (req, res) {
+  res.send('Hello World LORI ASHWOOD!!!!!!!');
+});
+app.listen(3000, function () {
+  console.log('Twitter app listening on port 3000!');
+});
