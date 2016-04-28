d0fae164a8da707c182e2476553f0e44c2335f74
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-04-28 10:23:14 -0400

    Set up Express, Node, Morgan, Swig and style with layout and index templates. Woot

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
+var swig = require('swig');
+var routes = require('./routes');
 
 app.use(morgan(':method :url :status'));
+app.engine('html', swig.renderFile);
+app.set('views',__dirname + '/views/' );
+app.set('view engine', 'html');
 
-app.use(function (req, res, next) {
-    // do your logging here
-    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
-    console.log(req.method, req.url)
-    next();
-})
-app.use("/special/", function (req, res, next){
-    console.log("You reached the special area");
-    next();
-})
+//Remove lines when deploying
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
+app.use('/', routes);
+app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!!!!!!!');
-});
 
-app.get('/news', function (req, res) {
-  res.send('Hello World LORI ASHWOOD!!!!!!!');
-});
+
+
 app.listen(3000, function () {
   console.log('Twitter app listening on port 3000!');
 });
-
-console.log("hello");
